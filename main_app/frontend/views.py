ec5fdca515ed46306345e8dfa0372f7ef2914401
Sebastian Giurca <sgiurca@jouve.com>
2022-03-15 14:51:16 +0200

    [U] Created webpack/babel config files, frontend app, react integration working

--- main_app/frontend/views.py
+++ main_app/frontend/views.py
@@ -1,11 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
-def main(request):
-    return HttpResponse("HELLO")
 
 def index(request):
     return render(request, 'frontend/index.html')
